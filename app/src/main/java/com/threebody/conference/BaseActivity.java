0c98781a23298bdbd6387b94224b279d51d66070
gateinsky <119143080@qq.com>
2015-01-13 13:09:50 +0800

    activity

---
+++ app/src/main/java/com/threebody/conference/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.threebody.conference;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by xiaxin on 15-1-13.
+ */
+public class BaseActivity extends ActionBarActivity {
+}
---
+++ app/src/main/java/com/threebody/conference/MeetingActivity.java
@@ -0,0 +1,7 @@
+package com.threebody.conference;
+
+/**
+ * Created by xiaxin on 15-1-13.
+ */
+public class MeetingActivity extends BaseActivity{
+}
