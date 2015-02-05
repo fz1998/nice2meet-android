9d9d44cbbdae8b2ec87cf4b076c93c0bdf9fb87e
XiaXin <119143080@qq.com>
2015-02-05 20:12:15 +0800

    .jar

--- ThreeBody/ConferenceSDK/src/main/java/com/threebody/sdk/listener/OnJoinConferenceListener.java
+++
@@ -1,8 +0,0 @@
-package com.threebody.sdk.listener;
-
-/**
- * Created by xiaxin on 15-1-13.
- */
-public interface OnJoinConferenceListener {
-    void onJoinResult();
-}
--- ThreeBody/app/src/main/java/com/threebody/conference/ui/fragment/VideoViewFragmenet.java
+++
@@ -1,7 +0,0 @@
-package com.threebody.conference.ui.fragment;
-
-/**
- * Created by xiaxin on 15-1-14.
- */
-public class VideoViewFragmenet {
-}
