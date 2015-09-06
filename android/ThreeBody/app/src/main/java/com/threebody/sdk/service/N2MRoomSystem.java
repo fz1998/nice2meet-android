f41130730f210082a50f3e37888f13de96476c55
fz1998 <734709472@qq.com>
2015-09-06 13:04:42 +0800

    N2MRoomSystem: deleted room list.

--- android/ThreeBody/app/src/main/java/com/threebody/sdk/service/N2MRoomSystem.java
+++ android/ThreeBody/app/src/main/java/com/threebody/sdk/service/N2MRoomSystem.java
@@ -3,9 +3,6 @@ package com.threebody.sdk.service;
 import org.st.Room;
 import org.st.RoomSystem;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by xiaxin on 15-2-5.
  */
@@ -14,8 +11,6 @@ public class N2MRoomSystem {
     private static N2MRoomSystem instance ;
 
     private RoomSystem roomSystem;
-
-    private List<RoomService> roomList;
 
     // TODO: 2015/9/6 allows only one conference room exist.
     private RoomService roomService;
@@ -28,12 +23,10 @@ public class N2MRoomSystem {
     }
     private N2MRoomSystem(){
         roomSystem = new RoomSystem();
-        roomList = new ArrayList<>();
     }
 
     public RoomService obtainRoom(String roomNumber) {
         this.roomService = new RoomService(roomNumber);
-        roomList.add(roomService);
         Room room = roomSystem.createRoom(roomService.getRoomListener(), roomNumber);
         this.roomService.setRoomModule(room);
         return roomService;
